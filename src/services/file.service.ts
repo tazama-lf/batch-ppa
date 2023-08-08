4b16b384ab59ec60f99342d043f5b5f2ccc6c6f1
cshezi <cshezi@sybrin.co.za>
2023-08-08 12:43:35 +0200

    fix: bug fix rl ending up being empty investigation memory loss of line

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -259,13 +259,6 @@ const sendPrepareTransaction = async (
 };
 
 export const SendLineMessages = async (requestBody: any): Promise<string> => {
-  const fileStream = fs.createReadStream('./uploads/input.txt');
-
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity,
-  });
-
   // Note: we use the crlfDelay option to recognize all instances of CR LF
   // ('\r\n') in input.txt as a single line break.
   if (
@@ -294,6 +287,13 @@ export const SendLineMessages = async (requestBody: any): Promise<string> => {
     oldestTimestamp = await dbService.getOldestTimestampPacs008();
     delta = Date.now() - new Date(oldestTimestamp).getTime();
   }
+
+  const fileStream = fs.createReadStream('./uploads/input.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
 
   for await (const line of rl) {
     // Each line in input.txt will be successively available here as `line`.
