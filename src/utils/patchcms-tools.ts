f69c51cc4f992899ea8cb54e3100549e1f621f1b
cshezi <cshezi@sybrin.co.za>
2023-11-14 15:11:40 +0200

    chore: further logging on the patch path

--- src/utils/patchcms-tools.ts
+++ src/utils/patchcms-tools.ts
@@ -4,6 +4,7 @@ import * as readline from 'readline';
 import { LoggerService } from '../logger.service';
 
 const sendMissingTransactionsCMS = async (): Promise<void> => {
+  LoggerService.error('Started Patching the batch from cms');
   const rl = readline.createInterface({
     input: fs.createReadStream('./uploads/input.txt'),
     crlfDelay: Infinity,
@@ -13,18 +14,23 @@ const sendMissingTransactionsCMS = async (): Promise<void> => {
     // Request report on the arango
     if (line.length < 30) {
       LoggerService.error(
-        'File is not a patch file or EndToEndId not equal to 33 long',
+        `Expected End to End id but got ${line} skiping this line `,
       );
-      return;
+      continue;
     }
+
+    LoggerService.error(`Transaction ${line} is being evaluated`);
 
     const report = (await dbService.getTransactionReport(
       line.trim(),
     )) as object;
 
+    LoggerService.error(`Transaction ${line} was sent to CMS for reporting`);
     // Send to CMS
     await natsServer.handleResponse({ ...report[0][0] });
   }
+
+  LoggerService.error('Finished Patching the batch from cms');
 };
 
 export default sendMissingTransactionsCMS;
